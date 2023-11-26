1a09360796e2ece6687cb0472721d7f165ae8954
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2023-11-26 23:06:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 let nam = document.getElementById("cont");
 let show = document.getElementById("contact");
 
+console.log(nam.innerHTML)
+
 var count = 0;
 
 nam.addEventListener("click", (e) => {
     if (count == 0) {
         show.style.display = "block";
-        show.classList.toggle("show");
-        nam.style.color = "red";
+        show.classList.add("show");
+        nam.innerHTML = `<i class="ri-feedback-line"></i>`;
+        nam.style.color = "aliceblue";
+        intervalId = setInterval(() => {
+            show.style.paddingTop = "3rem";
+        }, 100);
         count += 1;
     } else {
         show.style.display = "none";
         show.classList.remove("show");
-        nam.style.color = "black";
+        nam.innerHTML = `Contact`;
+        show.style.paddingTop = '2rem';
+        clearInterval(intervalId);
+        nam.style.color = "black";        
         count = count - 1;
     }
 
